3811cd0807376111a3ff9fe257a09952a71bc6c5
Melina Moraiti <melinamoraith@gmail.com>
2024-06-12 15:38:35 +0300

    Color each side of cube with a different shade of blue

--- js/script.js
+++ js/script.js
@@ -58,58 +58,99 @@ function initShaders() {
 function initBuffers() {
     var cubeVertices = new Float32Array([
         // Front face
-        0.3,  0.3,  0.3, 1.0,  // Vertex 0
-       -0.3,  0.3,  0.3, 1.0,  // Vertex 1
-       -0.3, -0.3,  0.3, 1.0,  // Vertex 2
-        0.3, -0.3,  0.3, 1.0,  // Vertex 3
-
+         0.3,  0.3,  0.3, 1.0,  // Vertex 0
+        -0.3,  0.3,  0.3, 1.0,  // Vertex 1
+        -0.3, -0.3,  0.3, 1.0,  // Vertex 2
+         0.3, -0.3,  0.3, 1.0,  // Vertex 3
         // Back face
-        0.3,  0.3, -0.3, 1.0,  // Vertex 4
-       -0.3,  0.3, -0.3, 1.0,  // Vertex 5
-       -0.3, -0.3, -0.3, 1.0,  // Vertex 6
-        0.3, -0.3, -0.3, 1.0   // Vertex 7
+         0.3,  0.3, -0.3, 1.0,  // Vertex 4
+        -0.3,  0.3, -0.3, 1.0,  // Vertex 5
+        -0.3, -0.3, -0.3, 1.0,  // Vertex 6
+         0.3, -0.3, -0.3, 1.0,  // Vertex 7
+        // Top face
+         0.3,  0.3, -0.3, 1.0,  // Vertex 8
+        -0.3,  0.3, -0.3, 1.0,  // Vertex 9
+        -0.3,  0.3,  0.3, 1.0,  // Vertex 10
+         0.3,  0.3,  0.3, 1.0,  // Vertex 11
+        // Bottom face
+         0.3, -0.3,  0.3, 1.0,  // Vertex 12
+        -0.3, -0.3,  0.3, 1.0,  // Vertex 13
+        -0.3, -0.3, -0.3, 1.0,  // Vertex 14
+         0.3, -0.3, -0.3, 1.0,  // Vertex 15
+        // Right face
+         0.3,  0.3, -0.3, 1.0,  // Vertex 16
+         0.3,  0.3,  0.3, 1.0,  // Vertex 17
+         0.3, -0.3,  0.3, 1.0,  // Vertex 18
+         0.3, -0.3, -0.3, 1.0,  // Vertex 19
+        // Left face
+        -0.3,  0.3, -0.3, 1.0,  // Vertex 20
+        -0.3,  0.3,  0.3, 1.0,  // Vertex 21
+        -0.3, -0.3,  0.3, 1.0,  // Vertex 22
+        -0.3, -0.3, -0.3, 1.0,  // Vertex 23
     ]);
-	vertexBuffer = gl.createBuffer(); 
-	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer); 
-	gl.bufferData(gl.ARRAY_BUFFER, cubeVertices,	gl.STATIC_DRAW); 
-	vertexBuffer.itemSize = 4;  
-	vertexBuffer.itemCount = 8;
+    vertexBuffer = gl.createBuffer(); 
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer); 
+    gl.bufferData(gl.ARRAY_BUFFER, cubeVertices, gl.STATIC_DRAW); 
+    vertexBuffer.itemSize = 4;  
+    vertexBuffer.itemCount = 24;
 
-	// Colors for 8 vertices
-	var cubeColors = new Float32Array([
-				1.0, 0.0, 0.0, 1.0, // Color of vertex 0: κόκκινο
-				0.0, 1.0, 0.0, 1.0, // Χρώμα κορυφής Β: πράσινο
-				0.0, 0.0, 1.0, 1.0, // Χρώμα κορυφής Γ: μπλε
-				1.0, 1.0, 0.0, 1.0,  // Χρώμα κορυφής Δ: κίτρινο
-                1.0, 1.0, 0.0, 1.0,  // Χρώμα κορυφής Δ: κίτρινο
-                1.0, 1.0, 0.0, 1.0,  // Χρώμα κορυφής Δ: κίτρινο
-                1.0, 1.0, 0.0, 1.0,  // Χρώμα κορυφής Δ: κίτρινο
-                1.0, 1.0, 0.0, 1.0  // Χρώμα κορυφής Δ: κίτρινο
-				]);
+		// Colors for 8 vertices
+    var cubeColors = new Float32Array([
+        // Front face - Light cyan
+        0.79, 0.94, 0.97, 1.0,  // Vertex 0
+        0.79, 0.94, 0.97, 1.0,  // Vertex 1
+        0.79, 0.94, 0.97, 1.0,  // Vertex 2
+        0.79, 0.94, 0.97, 1.0,  // Vertex 3
+
+        // Back face - Honolulu blue
+        0, 0.54, 0.71, 1.0,  // Vertex 4
+        0, 0.54, 0.71, 1.0,  // Vertex 5
+        0, 0.54, 0.71, 1.0,  // Vertex 6
+        0, 0.54, 0.71, 1.0,   // Vertex 7
+
+        // Top face - Vivid Sky blue
+        0.28, 0.79, 0.89, 1.0,  // Vertex 8
+        0.28, 0.79, 0.89, 1.0,  // Vertex 9
+        0.28, 0.79, 0.89, 1.0,  // Vertex 10
+        0.28, 0.79, 0.89, 1.0,  // Vertex 11
+
+        // Bottom face - federal blue
+        0.01, 0.02, 0.37, 1.0,  // Vertex 12
+        0.01, 0.02, 0.37, 1.0,  // Vertex 13
+        0.01, 0.02, 0.37, 1.0,  // Vertex 14
+        0.01, 0.02, 0.37, 1.0,  // Vertex 15
+
+        // Right face - Marian blue
+        0.01, 0.24, 0.54, 1.0,  // Vertex 16
+        0.01, 0.24, 0.54, 1.0,  // Vertex 17
+        0.01, 0.24, 0.54, 1.0,  // Vertex 18
+        0.01, 0.24, 0.54, 1.0,  // Vertex 19
+
+        // Left face - Non photo blue
+        0.56, 0.87, 0.93, 1.0,  // Vertex 20
+        0.56, 0.87, 0.93, 1.0,  // Vertex 21
+        0.56, 0.87, 0.93, 1.0,  // Vertex 22
+        0.56, 0.87, 0.93, 1.0   // Vertex 23
+
+    ]);
 	colorBuffer = gl.createBuffer(); 
 	gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer); 
 	gl.bufferData(gl.ARRAY_BUFFER, cubeColors, gl.STATIC_DRAW); 
 	colorBuffer.itemSize = 4;  
-	colorBuffer.itemCount = 8;
+	colorBuffer.itemCount = 24;
 
-	var cubeIndices = new Uint16Array([
-        // Front face
-        0, 1, 2,  0, 2, 3,
-        // Back face
-        4, 5, 6,  4, 6, 7,
-        // Top face
-        0, 1, 5,  0, 5, 4,
-        // Bottom face
-        2, 3, 7,  2, 7, 6,
-        // Right face
-        0, 3, 7,  0, 7, 4,
-        // Left face
-        1, 2, 6,  1, 6, 5
+    var cubeIndices = new Uint16Array([
+        0, 1, 2,  0, 2, 3,  // Front face
+        4, 5, 6,  4, 6, 7,  // Back face
+        8, 9, 10,  8, 10, 11,  // Top face
+        12, 13, 14,  12, 14, 15,  // Bottom face
+        16, 17, 18,  16, 18, 19,  // Right face
+        20, 21, 22,  20, 22, 23   // Left face
     ]);
-	indexBuffer = gl.createBuffer(); 
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer); 
-	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, cubeIndices, gl.STATIC_DRAW);
-	indexBuffer.itemCount = 36; 
+    indexBuffer = gl.createBuffer(); 
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer); 
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, cubeIndices, gl.STATIC_DRAW);
+    indexBuffer.itemCount = 36; 
 }
 
 function drawScene() { 
@@ -125,7 +166,7 @@ function drawScene() {
 	numStepAngle = numStepAngle*Math.PI/180.0; 
 	totalAngle += numStepAngle; 
 	glMatrix.mat4.fromXRotation(rotationXMatrix, totalAngle );  
-
+   
     // Create a translation matrix
 	var translationMatrix = new Float32Array(16);
 	var finalMatrix = new Float32Array(16);
